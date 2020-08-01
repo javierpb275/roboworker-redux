9e4189236a9aca6175b62ae3311c2835cbd17d17
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-01 19:24:41 +0200

    created constant for the earnCoins action

--- client/src/actions.js
+++ client/src/actions.js
@@ -1,4 +1,7 @@
+import { ADD_COINS } from './constants.js';
+
+//This action takes care of getting coins when the user clicks on the work button.
 export const earnCoins = (coin) => ({
-    type: 'ADD_COINS',
+    type: ADD_COINS,
     payload: coin
-})
+});
---
+++ client/src/constants.js
@@ -0,0 +1,3 @@
+
+//Type ADD_COINS for the earnCoins action:
+export const ADD_COINS = 'ADD_COINS';
