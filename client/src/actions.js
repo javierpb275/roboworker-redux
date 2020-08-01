293cad114f06e24190da0006ca7c7eb9295cdf03
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-01 19:41:50 +0200

    created setSearchField action for thSearchBox to search for a product and created constant CHANGE_SEARCH_FIELD for this action

--- client/src/actions.js
+++ client/src/actions.js
@@ -1,5 +1,11 @@
 import { ADD_COINS } from './constants.js';
 
+//This action takes care of searching for the product that the user types in the SearchBox component.
+export const setSearchField = (text) => ({
+    type: CHANGE_SEARCH_FIELD,
+    payload: text
+})
+
 //This action takes care of getting coins when the user clicks on the work button.
 export const earnCoins = (coin) => ({
     type: ADD_COINS,
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,6 @@
+//Type CHANGE_SEARCH_FIELD for the setSearchField action:
+export const CHANGE_SEARCH_FIELD = 'CHANGE_SEARCH_FIELD';
+
 
 //Type ADD_COINS for the earnCoins action:
 export const ADD_COINS = 'ADD_COINS';
