893fda43e19dff1062f65afeadb83a050c569574
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-02 15:34:23 +0200

    added thunkMiddleware to handle asynchronous actions (like when we do fetch(http..)) like AJAX calls...

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';//It allows us to provide our App with redux and pass down the store to our App
 import { createStore, applyMiddleware } from 'redux';//It allows us to create a store (javascript object that describes our app)
 import { createLogger } from 'redux-logger';//it listens to our action and log them in the console so we can debug easily
+import thunkMiddleware from 'redux-thunk';// handle asynchronous actions (like when we do fetch('http...) )
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -11,7 +12,7 @@ import { searchProduct } from './reducers';
 //const store = createStore(rootReducer);//rootReducer: reducer of our app where we combine all our reducers (createStore(): This is the way we create a store[state])
 
 const logger = createLogger();
-const store = createStore(searchProduct, applyMiddleware(logger));//since we only have one reducer for now we just add searchProduct reducer
+const store = createStore(searchProduct, applyMiddleware(thunkMiddleware, logger));//since we only have one reducer for now we just add searchProduct reducer
 //applyMidlleware is used to add it as a second parameter when we create the store and tell it what middleware we want to use.
 
 ReactDOM.render(
