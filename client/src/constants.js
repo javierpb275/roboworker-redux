e4b1cf8db42f0c9e3fa53bdbc0878d2a4d7be4e5
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-08-02 16:20:54 +0200

    created constants for the spendCoins action. Since it is a promise, which is asynchronous, we created pending, success and failed

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,7 @@
 //Type CHANGE_SEARCH_FIELD for the setSearchField action:
 export const CHANGE_SEARCH_FIELD = 'CHANGE_SEARCH_FIELD';
 
+//Because it is a promise, it has 3 states:
+export const SPEND_COINS_PENDING = 'SPEND_COINS_PENDING';//Pending: first time we send that request, we are waiting for the promise to return
+export const SPEND_COINS_SUCCESS = 'SPEND_COINS_SUCCESS';
+export const SPEND_COINS_FAILED = 'SPEND_COINS_FAILED';
